eabbd91ce3119d79d7d69224dc93b1323bfdb856
starsgo <1526560658@qq.com>
2023-08-21 16:50:52 +0800

    v1.0

---
+++ test.cpp
@@ -0,0 +1,18 @@
+#include "stdio.h"
+#include "iostream"
+
+
+class Demo{
+    private:
+    int age;
+    
+    
+    public:
+    Demo(int,char*);
+    void setage(int);
+    int getage();
+
+};
+int main(){
+   
+}
